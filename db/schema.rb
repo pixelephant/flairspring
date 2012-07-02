c88ff66fcac6137233b6218565eacaf8dfc9298d
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-07-02 16:42:08 +0200

    schema
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611144541) do
+ActiveRecord::Schema.define(:version => 20120628145001) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
     t.string   "additional"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "billing"
+    t.boolean  "billing",    :default => false, :null => false
     t.text     "name"
+    t.boolean  "default"
   end
 
   create_table "advantage_translations", :force => true do |t|
@@ -111,6 +112,17 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
   end
 
   add_index "contents", ["slug"], :name => "index_contents_on_slug"
+
+  create_table "coupons", :force => true do |t|
+    t.string   "code"
+    t.boolean  "used",        :default => false, :null => false
+    t.integer  "offer_value"
+    t.integer  "offer_type"
+    t.date     "valid_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "order_id"
+  end
 
   create_table "custom_categories", :force => true do |t|
     t.string   "name"
@@ -252,6 +264,8 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
+    t.integer  "price"
   end
 
   create_table "orders", :force => true do |t|
@@ -261,6 +275,11 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
     t.text     "basket_serialization"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
+    t.date     "estimated_date"
+    t.integer  "payment_type"
+    t.integer  "discount_used"
+    t.integer  "price"
   end
 
   create_table "photos", :force => true do |t|
@@ -344,7 +363,7 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
     t.integer  "property_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "numeric"
+    t.float    "num"
   end
 
   create_table "properties_to_categories", :force => true do |t|
@@ -483,6 +502,7 @@ ActiveRecord::Schema.define(:version => 20120611144541) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
+    t.string   "accounting_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
